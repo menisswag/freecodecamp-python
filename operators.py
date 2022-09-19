34e5a06cc5b3affe68dd8e63c77f9889d9027620
Men <x2leonardopalhares@gmail.com>
2022-09-19 19:29:25 -0300

    add operators.py

---
+++ operators.py
@@ -0,0 +1,22 @@
+"""
+Existem quatro operadores primários
+soma = +
+subtraçãp = -
+divisão = /
+multiplicação = *
+"""
+
+mult = 3 * 3
+soma = 2 + 2
+sub = 3 - 2
+divi = 10 / 1
+
+print(mult)
+print(soma)
+print(sub)
+print(divi)
+
+# Cacule 4 vezes 5
+yes = 4 * 5
+print(yes)
+
