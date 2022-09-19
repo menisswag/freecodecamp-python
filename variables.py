846b14eb6915d703ebe216b4b750a142bc515494
Men <x2leonardopalhares@gmail.com>
2022-09-19 19:22:04 -0300

    add variables.py

---
+++ variables.py
@@ -0,0 +1,24 @@
+"""
+Variável -> Uma variável é um espaço na memória que uasmos para armazenar algum dado
+
+Tipos de dados primáris:
+
+char -> 'k'
+string -> "Koe"  -> Lista de characheteres
+int ->  10, 5
+float -> 2.5, 0.5
+"""
+
+number = 10 # int
+print(type(number))
+
+name = 'Eduardo'
+print(name)
+print(type(name))
+
+number = 20 
+print(number)
+
+number = 0.5
+print(type(number))
+print(number)
