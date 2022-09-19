786996f9618bbbac841efb7359c41ef82c2c5a3d
Men <x2leonardopalhares@gmail.com>
2022-09-19 19:21:44 -0300

    add hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Comentário -> Tudo que estiver no comentário não será compliado
+
+print("Hello World") # Exibe na tela 
+print("hello my name is men")
+print("i am a starter scripter thank u")
+print("i am here for my train :) ")
